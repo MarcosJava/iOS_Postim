d77bf096bc104678e48ea6911fd369394b713083
Marcos Felipe Souza <mfelipesp@gmail.com>
2017-10-28 19:23:34 -0200

    Initial Commit

---
+++ Postim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Postim
+//
+//  Created by Marcos Felipe Souza on 28/10/2017.
+//  Copyright © 2017 Marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
